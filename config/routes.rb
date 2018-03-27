ecef3707d9bfdb4bfa20c3372dafeda5144a67bb
louis delon <louleman34@gmail.com>
2018-03-27 18:10:12 +0200

    init routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sellers, only: [:new, :create, :edit, :update, :show] do
+    resources :offers
+  end
+  resources :buyers, only: [:new, :create, :edit, :update, :show]
+
+  root to: 'pages#home'
+
 end
