8286b8ddccf7bc16a4c6b7ec81412bb5d243fcdd
LouisDelon <louleman34@gmail.com>
2018-05-09 13:59:18 +0200

    setup the reservatrion controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,8 @@
 class ReservationsController < ApplicationController
 
   skip_before_action :authenticate_seller!
+  before_action :set_buyer, only: [:create, :edit, :update, :destroy]
+  before_action :set_reservation, only: [:edit, :update, :destroy]
 
   def index
     @buyer = current_buyer
@@ -14,8 +16,9 @@ class ReservationsController < ApplicationController
 
   def create
     @reservation = reservation.new
+    authorize @reservation
     if reservation.save?
-      redirect_to
+      redirect_to buyer_reservations_path(@buyer)
     else
       render :new
     end
@@ -25,9 +28,22 @@ class ReservationsController < ApplicationController
   end
 
   def update
+    @reservation.update
+    redirect_to buyer_reservations_path(@buyer)
   end
 
   def destroy
+    @reservation.destroy
+  end
+
+  private
+
+  def set_buyer
+    @buyer = Buyer.find(params[:buyer_id])
+  end
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
   end
 
 end
