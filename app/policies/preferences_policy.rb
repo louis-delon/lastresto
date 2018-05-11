03ea6d93d5252117f67016d1816cb14e061b37f1
LouisDelon <louleman34@gmail.com>
2018-05-11 19:52:45 +0200

    setup preferences pundit policy

--- app/policies/preferences_policy.rb
+++ app/policies/preferences_policy.rb
@@ -7,25 +7,33 @@ class PreferencesPolicy < ApplicationPolicy
   end
 
   def new?
+    create?
   end
 
   def create?
-
+    true
   end
 
   def show?
+    current_buyer?
   end
 
   def update?
+    current_buyer?
   end
 
   def edit?
+    update?
   end
 
   def destroy?
+    update?
   end
 
   private
 
+  def current_buyer_is_owner?
+    @buyer = @record.buyer
+  end
 
 end
