6321fe3ee9cecc8b13d6b335f006cdc019ea08a8
LouisDelon <louleman34@gmail.com>
2018-05-17 22:57:30 +0200

    fix some buggs in preference views

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -2,6 +2,7 @@ class PreferencesController < ApplicationController
 
   before_action :set_buyer
   before_action :set_preference, only: [:edit, :destroy]
+  before_action :available_preferences, only: [:new, :create]
   skip_before_action :authenticate_seller!
 
   def index
@@ -11,25 +12,11 @@ class PreferencesController < ApplicationController
   end
 
   def new
-    @categories = Category.all
-    @list_of_all_category_id = @categories.map { |category| category.id }
-    @preferences = Preference.all
-    @buyer_selected_preferences = @preferences.where(buyer_id: current_buyer.id)
-    @list_of_selected_category_id = @buyer_selected_preferences.map { |preference| preference.category_id }
-    @list_of_not_selected_category_id = @list_of_all_category_id - @list_of_selected_category_id
     @preference = Preference.new
     authorize @preference
   end
 
   def create
-
-    @categories = Category.all
-    @list_of_all_category_id = @categories.map { |category| category.id }
-    @preferences = Preference.all
-    @buyer_selected_preferences = @preferences.where(buyer_id: current_buyer.id)
-    @list_of_selected_category_id = @buyer_selected_preferences.map { |preference| preference.category_id }
-    @list_of_not_selected_category_id = @list_of_all_category_id - @list_of_selected_category_id
-
     @preference = Preference.new(params_preferences)
     @preference.buyer_id = @buyer.id
 
@@ -59,6 +46,20 @@ class PreferencesController < ApplicationController
 
   def set_preference
     @preference = Preference.find(params[:id])
+  end
+
+  def available_preferences
+    # allow to have a list of category_id of preferences that have been selected/not selected by the buyer
+    # list of categories id that can be used to build a preference
+    @categories = Category.all
+    @list_of_all_category_id = @categories.map { |category| category.id }
+    # list of preferences already selected by the buyer
+    @preferences = Preference.all
+    @buyer_selected_preferences = @preferences.where(buyer_id: current_buyer.id)
+    # list of preferences selected by a buyer dispalyed by category_id
+    @list_of_selected_category_id = @buyer_selected_preferences.map { |preference| preference.category_id }
+    # list of preferences not selected by a buyer displayed by category_id
+    @list_of_not_selected_category_id = @list_of_all_category_id - @list_of_selected_category_id
   end
 
 end
